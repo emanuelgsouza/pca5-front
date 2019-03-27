3ad68b0b04ee8c0779a067f50767877f4f3e8b45
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-03-27 20:36:05 -0300

    init configs and added dev npm script

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,9 +2,11 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MyLayout.vue'),
+    component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: '', component: () => import('pages/Index.vue')
+      }
     ]
   }
 ]
