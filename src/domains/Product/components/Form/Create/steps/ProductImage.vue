0adb51edfee463d3f1848972ce78030e32099016
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-05-02 21:38:36 -0300

    init setup Upload to Firebase

--- src/domains/Product/components/Form/Create/steps/ProductImage.vue
+++ src/domains/Product/components/Form/Create/steps/ProductImage.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="product-image">
+    <!-- <QUploader /> -->
+    Aqui vai uma imagem
+
+    <AppFirebaseUploader />
   </div>
 </template>
 
 <script>
 import StepsMixin from '../mixins/steps'
+import AppFirebaseUploader from 'src/components/AppFirebaseUploader'
 
 export default {
   name: 'ProductImage',
   mixins: [ StepsMixin ],
+  components: { AppFirebaseUploader },
   mounted () {
     if (this.isOnlineProduct) {
       this.$emit('nextStep')
