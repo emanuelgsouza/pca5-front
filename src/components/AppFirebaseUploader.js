19178511e6548d9afa7b965bb94c7271dc2e517e
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-05-05 15:57:57 -0300

    finished component to upload image to firebase

--- src/components/AppFirebaseUploader.js
+++ src/components/AppFirebaseUploader.js
@@ -1,16 +1,23 @@
 // MyUploader.js
 import { QUploaderBase } from 'quasar'
+import { first, last, isFunction } from 'lodash'
 import uuid from 'uuid'
 import storage from 'src/services/firebase/storage'
 
+const getFileExtension = imageType => last(imageType.split('/'))
+
 export default {
-  name: 'MyUploader',
+  name: 'FirebaseUploader',
   mixins: [ QUploaderBase ],
+  data: () => ({
+    internalFileRef: null,
+    isUploadingFilesToFirebase: false
+  }),
   computed: {
     // [REQUIRED]
     // we're working on uploading files
     isUploading () {
-      // return <Boolean>
+      return this.isUploadingFilesToFirebase
     },
 
     // [optional]
@@ -23,13 +30,20 @@ export default {
     // Use this.files like reference
     internalFiles () {
       return this.files.map(file => {
+        const fileExtension = getFileExtension(file.type)
+        const filename = `${uuid.v4()}.${fileExtension}`
         return {
-          ref: storage.ref().child(uuid.v4()),
+          file,
+          filename: filename,
+          ref: storage.ref().child(filename),
           metadata: {
             contentType: file.type
           }
         }
       })
+    },
+    internalFile () {
+      return first(this.internalFiles)
     }
   },
   methods: {
@@ -37,19 +51,33 @@ export default {
     // abort and clean up any process
     // that is in progress
     abort () {
-      // ...
-    },
-
-    // [REQUIRED]
-    upload () {
-      if (this.canUpload === false) {
-
+      if (this.internalFileRef && isFunction(this.internalFileRef.cancel)) {
+        this.internalFileRef.cancel()
       }
+    },
+    // [REQUIRED]
+    async upload () {
+      this.internalFileRef = this.internalFile.ref.put(this.internalFile.file)
 
-      console.log(this)
+      this.isUploadingFilesToFirebase = true
 
-      console.log(this.internalFiles)
-      // ...
+      this.internalFileRef.on('state_changed', snapshot => {
+        this.uploadedSize = snapshot.bytesTransferred
+      }, err => {
+        this.$q.notify({
+          message: 'Não foi possível subir a imagem para o nosso serviço',
+          color: 'negative'
+        })
+        this.isUploadingFilesToFirebase = false
+        console.error(err)
+      }, () => {
+        this.internalFileRef.snapshot.ref.getDownloadURL().then(downloadURL => {
+          console.log('File available at', downloadURL)
+          this.$emit('downloadURL', downloadURL)
+        })
+
+        this.isUploadingFilesToFirebase = false
+      })
     }
   }
 }
