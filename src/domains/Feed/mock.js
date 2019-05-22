6144001628d46bbde4e1c8ecbc896fa7b04b232f
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-05-22 20:57:48 -0300

    add mock to feed

---
+++ src/domains/Feed/mock.js
@@ -0,0 +1,56 @@
+export const feedDataMock = {
+  data: [
+    {
+      name: 'Nome do Produto',
+      is_online: false,
+      value: 40,
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur asperiores eum quasi iusto vitae ratione, consequuntur dolor ad quas eaque magnam nulla fugiat. Adipisci illum esse modi? Nisi, voluptatum sint.',
+      user: {
+        name: 'Ademar'
+      },
+      createdAt: new Date(),
+      category: {
+        name: 'Vestuário'
+      }
+    },
+    {
+      name: 'Nome do Produto',
+      is_online: true,
+      value: 40,
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur asperiores eum quasi iusto vitae ratione, consequuntur dolor ad quas eaque magnam nulla fugiat. Adipisci illum esse modi? Nisi, voluptatum sint.',
+      user: {
+        name: 'Ciclano'
+      },
+      createdAt: new Date(),
+      category: {
+        name: 'Vestuário'
+      }
+    },
+    {
+      name: 'Nome do Produto',
+      is_online: false,
+      value: 40,
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur asperiores eum quasi iusto vitae ratione, consequuntur dolor ad quas eaque magnam nulla fugiat. Adipisci illum esse modi? Nisi, voluptatum sint.',
+      user: {
+        name: 'Emanuel'
+      },
+      createdAt: new Date(),
+      category: {
+        name: 'Vestuário'
+      }
+    },
+    {
+      name: 'Nome do Produto',
+      is_online: false,
+      value: 40,
+      description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequatur asperiores eum quasi iusto vitae ratione, consequuntur dolor ad quas eaque magnam nulla fugiat. Adipisci illum esse modi? Nisi, voluptatum sint.',
+      user: {
+        name: 'Thanos'
+      },
+      createdAt: new Date(),
+      category: {
+        name: 'Vestuário'
+      }
+    }
+  ]
+}
